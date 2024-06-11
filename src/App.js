99aeedaea46e0a6ccaa7852ec95b8f86cbb7497c
Marta Prystatska <tradlisboa@gmail.com>
2024-06-11 08:18:13 +0100

    Add boolean props with out value

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Ciao from "./components/Ciao";
 function App() {
   return (
     <>
-      <Ciao name="Fred" age={43} isMale={true} />
-      <Ciao name="Bred" age={45} isMale={true} />
-      <Ciao name="Ana" age={27} isMale={false} />
+      <Ciao name="Fred" age={43} isMale />
+      <Ciao name="Bred" age={45} isMale />
+      <Ciao name="Ana" age={27} />
     </>
   );
 }
