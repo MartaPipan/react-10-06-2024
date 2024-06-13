e0e16f53df58c2e322e0f76cda75d414a735f3c8
Marta Prystatska <tradlisboa@gmail.com>
2024-06-13 09:52:23 +0100

    Task: SortByName(button)

--- src/components/CiaoList/index.jsx
+++ src/components/CiaoList/index.jsx
@@ -25,28 +25,49 @@ class CiaoList extends Component {
           isMale: false,
         },
       ],
-      isUpSort: true,
+      isUpSortByAge: true,
+      isUpSortByName: true,
+      
     };
   }
+  sortUsersByName = () => {
+    const { users, isUpSortByName } = this.state;
+    this.setState({
+      users: users.toSorted((userA, userB) => {
+        const userNameA = userA.name.toUpperCase(); // ignore upper and lowercase
+        const userNameB = userB.name.toUpperCase(); // ignore upper and lowercase
+        if (userNameA < userNameB) {
+          return isUpSortByName?-1:1;
+        }
+        if (userNameA > userNameB) {
+          return isUpSortByName ? 1:-1;
+        }
+        // names must be equal
+        return 0;
+      }),
+      isUpSortByName:!isUpSortByName,
+    })
+  }
   sortUsersByAge = () => {
-    const { users, isUpSort} = this.state; //users - variable
+    const { users, isUpSortByAge} = this.state; //users - variable
     // поверхнева копія використовується для об'єктів першого порядку
     // об'єкти першого порядку - це такі об'єкти значеннями властивостей яких є примітиви
     this.setState({
       users: users.toSorted((userA, userB) =>
-        isUpSort ? userA.age - userB.age : userB.age - userA.age
+        isUpSortByAge ? userA.age - userB.age : userB.age - userA.age
       ),
-      isUpSort: !isUpSort,
+      isUpSortByAge: !isUpSortByAge,
     });
   };
   mapUsers = ({ id, name, age, isMale },i) => (
     <Ciao key={id} name={name} age={age} isMale={isMale} />
   );
   render() {
-    const { users,isUpSort} = this.state;
+    const { users,isUpSortByAge,isUpSortByName} = this.state;
     return (
       <>
-        <button onClick={this.sortUsersByAge}>sort{isUpSort?'Up':'Down'} by age</button>
+        <button onClick={this.sortUsersByName}>sort{isUpSortByName?'Up':'Down'} by name</button>
+        <button onClick={this.sortUsersByAge}>sort{isUpSortByAge?'Up':'Down'} by age</button>
         <section>{users.map(this.mapUsers)}</section>;
       </>
     );
