db9394afe38aeddf46f2ea593382efedb53a55c6
Marta Prystatska <tradlisboa@gmail.com>
2024-06-13 08:25:22 +0100

    o que pode ser key? key=id(prop), key=index. add variables copyState=STATE IMUTAVEL(1.toSorted 2.copy ) 3.used copia from JSON!!!!!!!

--- src/components/CiaoList/index.jsx
+++ src/components/CiaoList/index.jsx
@@ -27,15 +27,31 @@ class CiaoList extends Component {
       ],
     };
   }
+  sortUsersByAge = () => {
+    const { users } = this.state; //users - variable
+    //створити копію масива і вже новий масив встановити в стан!!!
+    //мутувати стан заборонено!!!
+    //1 спосіб - поверхнева копія за допомогою спред оператора
+    //const usersCopy = [...users];
+    //usersCopy.sort((userA, userB) => userA.age - userB.age);
+    //2 спосіб - поверхнева копія за допомогою toSorted
+    //const usersCopy = users.toSorted((userA, userB) => userA.age - userB.age);
+    //3 спосіб - глибока копія за допомогою JSON
+    const usersCopy = JSON.parse(JSON.stringify(users));
+    usersCopy.sort((userA, userB) => userA.age - userB.age);
+    this.setState({ users: usersCopy });
+  };
   mapUsers = ({ id, name, age, isMale }, index) => (
-    //<li key={id}>
-    <li key={index}> substituir id obrigatorio  por key index 
-      <Ciao name={name} age={age} isMale={isMale} />
-    </li>
+    <Ciao key={index} name={name} age={age} isMale={isMale} />
   );
   render() {
     const { users } = this.state;
-    return <ul>{users.map(this.mapUsers)}</ul>;
+    return (
+      <>
+        <button onClick={this.sortUsersByAge}>sort by age</button>
+        <section>{users.map(this.mapUsers)}</section>;
+      </>
+    );
   }
 }
 
