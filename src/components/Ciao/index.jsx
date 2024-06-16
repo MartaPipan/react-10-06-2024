9e25fcd770375c27a5106dab1feec9d78f11453f
Marta Prystatska <tradlisboa@gmail.com>
2024-06-16 20:56:57 +0100

    add parent-child props,child-parent callback

--- src/components/Ciao/index.jsx
+++ src/components/Ciao/index.jsx
@@ -1,29 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Ciao extends Component {
-    constructor() {
-        super();
-        this.state = {
-            isHi: true,
-            isSelect: false,
-        };
-    }
-    handlerClickInButton = () => {
-        const { isHi } = this.state;
-        this.setState({ isHi: !isHi })
-    }
-    render() {
-        const { isHi } = this.state;
-        console.log(this.props);
-        const { name, age, isMale } = this.props;
-        return (
-            <article>
-                <h2>{isHi ? 'Hi' : 'Bye'}, {name}! ({age})</h2>
-                <button onClick={this.handlerClickInButton}>switch</button>
-                <p>gender:{isMale ? 'male' : 'female'}</p>
-            </article>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      isHi: true,
+    };
+  }
+  handlerClickInButton = () => {
+    const { isHi } = this.state;
+    this.setState({ isHi: !isHi });
+  };
+    
+  render() {
+    const { isHi } = this.state;
+    console.log(this.props);
+    const {
+      user: { name,surname, age, isMale, isSelected, id },
+      userSelected,
+    } = this.props;
+    const colorBG = isSelected ? "aqua" : "#eee";
+    return (
+      <article
+        onClick={() => {
+          userSelected(id);
+        }}
+        style={{ backgroundColor: colorBG }}
+      >
+        <h2>
+          {isHi ? "Hi" : "Bye"}, {name} {surname}! ({age})
+        </h2>
+        <button onClick={this.handlerClickInButton}>switch</button>
+        <p>gender:{isMale ? "male" : "female"}</p>
+      </article>
+    );
+  }
 }
 
 export default Ciao;
--- src/components/CiaoList/index.jsx
+++ src/components/CiaoList/index.jsx
@@ -1,75 +1,79 @@
 import React, { Component } from "react";
 import Ciao from "../Ciao";
 
+const USERS_FROM_DB = [
+  {
+    id: 6,
+    name: "Fred",
+    surname: "Gray",
+    age: 45,
+    isMale: true,
+  },
+  {
+    id: 2,
+    name: "Bred",
+    surname: "Pit",
+    age: 51,
+    isMale: true,
+  },
+  {
+    id: 23,
+    name: "Anna",
+    surname: "Fer",
+    age: 24,
+    isMale: false,
+  },
+];
+
 class CiaoList extends Component {
   constructor() {
     super();
     this.state = {
-      users: [
-        {
-          id: 6,
-          name: "Fred",
-          surname:'Gray',
-          age: 45,
-          isMale: true,
-        },
-        {
-          id: 2,
-          name: "Bred",
-          surname:'Pit',
-          age: 51,
-          isMale: true,
-        },
-        {
-          id: 23,
-          name: "Anna",
-          surname:'Fer',
-          age: 24,
-          isMale: false,
-        },
-      ],
+      users: USERS_FROM_DB.map((user) => ({ ...user, isSelected: false })),
       isUpSortByAge: true,
       isUpSortByName: true,
-      
+      isUpSortBySurname: true,
     };
   }
+
   sortUsersByName = () => {
     const { users, isUpSortByName } = this.state;
     this.setState({
-      users: users.toSorted((userA, userB) => {    //sort like toSorted exemple in https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+      users: users.toSorted((userA, userB) => {
+        //sort like toSorted exemple in https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
         const userNameA = userA.name.toUpperCase(); // ignore upper and lowercase
         const userNameB = userB.name.toUpperCase(); // ignore upper and lowercase
         if (userNameA < userNameB) {
-          return isUpSortByName?-1:1;
+          return isUpSortByName ? -1 : 1;
         }
         if (userNameA > userNameB) {
-          return isUpSortByName ? 1:-1;
+          return isUpSortByName ? 1 : -1;
         }
         // names must be equal
         return 0;
       }),
-      isUpSortByName:!isUpSortByName,
-    })
-  }
-   sortUsersBySurname = () => {
+      isUpSortByName: !isUpSortByName,
+    });
+  };
+  sortUsersBySurname = () => {
     const { users, isUpSortBySurname } = this.state;
     this.setState({
-      users: users.toSorted((userA, userB) => {  
+      users: users.toSorted((userA, userB) => {
         const userSurnameA = userA.surname.toUpperCase(); // ignore upper and lowercase
         const userSurnameB = userB.surname.toUpperCase(); // ignore upper and lowercase
         if (userSurnameA < userSurnameB) {
-          return isUpSortBySurname?-1:1;
+          return isUpSortBySurname ? -1 : 1;
         }
         if (userSurnameA > userSurnameB) {
-          return isUpSortBySurname ? 1:-1;
+          return isUpSortBySurname ? 1 : -1;
         }
         return 0;
       }),
-      isUpSortBySurname:!isUpSortBySurname,
-    })
-  }
+      isUpSortBySurname: !isUpSortBySurname,
+    });
+  };
   sortUsersByAge = () => {
-    const { users, isUpSortByAge} = this.state; //users - variable
+    const { users, isUpSortByAge } = this.state; //users - variable
     // поверхнева копія використовується для об'єктів першого порядку
     // об'єкти першого порядку - це такі об'єкти значеннями властивостей яких є примітиви
     this.setState({
@@ -79,16 +83,50 @@ class CiaoList extends Component {
       isUpSortByAge: !isUpSortByAge,
     });
   };
-  mapUsers = ({ id, name,surname, age, isMale },i) => (
-    <Ciao key={id} name={name} surname={surname} age={age} isMale={isMale} />
+
+  userSelected = (id) => {
+    const { users } = this.state;
+    this.setState({
+      users: users.map((user) => ({
+        ...user,
+        isSelected: user.id === id ? !user.isSelected : user.isSelected,
+      })),
+    });
+  };
+
+  //mapUsers = ({ id, name, surname, age, isMale, isSelected }, i) => (
+  //  <Ciao
+  //    key={id}
+  //    name={name}
+  //    surname={surname}
+  //    age={age}
+  //    isMale={isMale}
+  //    isSelected={isSelected}
+  //    id={id}
+  //    userSelected={this.userSelected}
+  //  />
+  //);
+  mapUsers = (user) => (
+    <Ciao
+      key={user.id}
+      user={user}
+      userSelected={this.userSelected}
+    />
   );
   render() {
-    const { users,isUpSortByAge,isUpSortByName,isUpSortBySurname} = this.state;
+    const { users, isUpSortByAge, isUpSortByName, isUpSortBySurname } =
+      this.state;
     return (
       <>
-        <button onClick={this.sortUsersByName}>sort{isUpSortByName ? 'Up' : 'Down'} by name</button>
-        <button onClick={this.sortUsersBySurname}>sort{isUpSortBySurname?'Up':'Down'} by surname</button>
-        <button onClick={this.sortUsersByAge}>sort{isUpSortByAge?'Up':'Down'} by age</button>
+        <button onClick={this.sortUsersByName}>
+          sort{isUpSortByName ? "Up" : "Down"} by name
+        </button>
+        <button onClick={this.sortUsersBySurname}>
+          sort{isUpSortBySurname ? "Up" : "Down"} by surname
+        </button>
+        <button onClick={this.sortUsersByAge}>
+          sort{isUpSortByAge ? "Up" : "Down"} by age
+        </button>
         <section>{users.map(this.mapUsers)}</section>;
       </>
     );
