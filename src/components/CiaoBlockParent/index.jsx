f6bfd10678ccc9a5c6c42580650e5a57551d7f73
Marta Prystatska <tradlisboa@gmail.com>
2024-06-17 00:44:34 +0100

    rename CiaoListParent for CiaoBlockParent

---
+++ src/components/CiaoBlockParent/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CiaoBlockParent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Index;
